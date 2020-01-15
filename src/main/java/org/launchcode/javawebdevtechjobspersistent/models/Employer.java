948f9febe4186398c2410577f375ac5971de09d5
Carly Langlois <cj.langlois.cl@gmail.com>
2020-01-15 15:46:30 -0600

    add onetomany annotation

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -13,6 +13,9 @@ public class Employer extends AbstractEntity {
     @Size(min=3, max=15)
     private String location;
 
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {
     }
